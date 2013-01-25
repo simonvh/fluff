c926eb386286791ff56005a12c6535194d26b01c
Simon van Heeringen <simon.vanheeringen@gmail.com>
2013-01-25 08:27:21 +0100

    Color stuff

--- fluff/color.py
+++ fluff/color.py
@@ -5,6 +5,9 @@
 # This script is free software. You can redistribute it and/or modify it under 
 # the terms of the MIT License
 
+COLOR_MAP = {"red":"#e41a1c","blue":"#377eb8","green":"#4daf4a","orange":"#ff7f00","brown":"#a65628","purple":"#984ea3","yellow":"#ffff33"}
+DEFAULT_COLORS = "#e41a1c,#377eb8,#4daf4a,#984ea3,#ff7f00,#ffff33,#a65628"
+
 def create_colormap(col1, col2):
 	c1 = colorConverter.to_rgb(col1)
 	c2 = colorConverter.to_rgb(col2)
