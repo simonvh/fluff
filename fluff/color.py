4a5ddc1178ed89cd583b717271093df3dde38c53
Simon <simon.vanheeringen@gmail.com>
2013-01-25 10:06:47 +0100

    import

--- fluff/color.py
+++ fluff/color.py
@@ -5,6 +5,8 @@
 # This script is free software. You can redistribute it and/or modify it under 
 # the terms of the MIT License
 
+from matplotlib.colors import colorConverter, LinearSegmentedColormap
+
 COLOR_MAP = {"red":"#e41a1c","blue":"#377eb8","green":"#4daf4a","orange":"#ff7f00","brown":"#a65628","purple":"#984ea3","yellow":"#ffff33"}
 DEFAULT_COLORS = "#e41a1c,#377eb8,#4daf4a,#984ea3,#ff7f00,#ffff33,#a65628"
 
