0b675b89508801b5daf44b00a6559e7bbe908386
simonvh <simon.vanheeringen@gmail.com>
2014-12-05 13:30:21 +0100

    at least basic working version

--- fluff/fluffio.py
+++ fluff/fluffio.py
@@ -330,6 +330,7 @@ def get_binned_stats(in_fname, data_fname, nbins, rpkm=False, rmdup=False, rmrep
         in_track = pybedtools.BedTool(in_fname)
     extend = fragmentsize - readlength
     for feature, min_strand, plus_strand in track.fetch_to_counts(in_track, rmdup, rmrepeats):
+        min_strand = []
         binsize = (feature.end - feature.start) / float(nbins)
         row = []
         overlap = []
@@ -345,10 +346,10 @@ def get_binned_stats(in_fname, data_fname, nbins, rpkm=False, rmdup=False, rmrep
             pos = int(bin_start + binsize + 0.5)
             
             if len(min_strand) > 0:
-                for c in range(len(min_strand) - 1, -1, -1):
+                for c in range(0,len(min_strand)):
                     if min_strand[c] + fragmentsize > pos:
                         break
-                min_strand = min_strand[:c + 1]
+                min_strand = min_strand[c:]
 
             i = 0
             while i < len(plus_strand) and plus_strand[i] <= int(bin_start + binsize + 0.5):
@@ -359,7 +360,7 @@ def get_binned_stats(in_fname, data_fname, nbins, rpkm=False, rmdup=False, rmrep
                 for c in range(0, len(plus_strand)):
                     if plus_strand[c] + fragmentsize > pos:
                         break
-                plus_strand = plus_strand[:c]
+                plus_strand = plus_strand[c:]
        
             if rpkm:
                 per_kb = num_reads * (1000.0 / binsize)
