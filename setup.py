0bda78297ab83c08723debbf3bfd406fc9036698
George Georgiou <george@science.ru.nl>
2016-01-15 13:22:09 +0100

    Optimized imports, readthedocs

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from distutils.core import setup
-import setuptools
+
 from fluff.config import FL_VERSION
 
 DESCRIPTION = """
 fluff - plots and graphs 
 """
 
-setup (name = 'fluff',
-        version = FL_VERSION,
-        description = DESCRIPTION,
-        author='Simon van Heeringen',
-        author_email='s.vanheeringen@ncmls.ru.nl',
-        license='MIT',
-        packages=[
-            'fluff',
-            'fluff/commands'
-        ],
-        scripts=[
-            "scripts/fluff_bandplot.py",
-            "scripts/fluff_profile.py",
-            "scripts/fluff_heatmap.py",
-            "scripts/fluff",
-        ],
-        data_files=[]
-)
+setup(name='fluff',
+      version=FL_VERSION,
+      description=DESCRIPTION,
+      author='Simon van Heeringen',
+      author_email='s.vanheeringen@ncmls.ru.nl',
+      license='MIT',
+      packages=[
+          'fluff',
+          'fluff/commands'
+      ],
+      scripts=[
+          "scripts/fluff_bandplot.py",
+          "scripts/fluff_profile.py",
+          "scripts/fluff_heatmap.py",
+          "scripts/fluff",
+      ],
+      data_files=[]
+      )
