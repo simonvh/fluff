781d6dd1cda5fddd6adde00b99e8e89304054746
George Georgiou <george@science.ru.nl>
2016-01-20 14:48:10 +0100

    Setup dependencies

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ fluff - plots and graphs
 setup(name='fluff',
       version=FL_VERSION,
       description=DESCRIPTION,
-      author='Simon van Heeringen',
-      author_email='s.vanheeringen@ncmls.ru.nl',
+      author='Georgios Georgiou',
+      author_email='g.georgiou@science.ru.nl',
       license='MIT',
       packages=[
           'fluff',
@@ -22,5 +22,16 @@ setup(name='fluff',
           "scripts/fluff_heatmap.py",
           "scripts/fluff",
       ],
-      data_files=[]
+      data_files=[],
+      install_requires=[
+        "pysam",
+        "pybedtools",
+        "HTSeq",
+        "matplotlib >= 1.1.1",
+        "numpy >= 1.6.0",
+        "scipy >= 0.9.0",
+        "colorbrewer",
+        "Pycluster >= 1.52",
+        "pp >= 1.6",
+      ],
       )
