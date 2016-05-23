ec013fa53433325d5e5ded03d0c87a1d5b21eeab
Simon van Heeringen <simon.vanheeringen@gmail.com>
2016-05-23 12:47:18 +0200

    fixed test

--- tests/test_track.py
+++ tests/test_track.py
@@ -61,30 +61,15 @@ def test_binned_stats(tracks, interval, region):
     for t in tracks:
         print t
         bla = [list(interval) + bins[t.track_type]]
-        assert bla == [x for x in t.binned_stats(region, 10, split=True)]
+        if t.track_type == "profile":
+            assert bla == [x for x in t.binned_stats(region, 10, split=True, statistic="max")]
+        else:
+            assert bla == [x for x in t.binned_stats(region, 10, split=True)]
 
-def test_bamtrack(bamfile, bedfile, wigfile, bwfile, interval, region):
-    from fluff.track import BamTrack, BedTrack, WigTrack, BigWigTrack
+def test_fetch(tracks, interval, region):
     
-    # TODO: check if not [2,0,0,0,1,1,2,3,3,3]
-    bins = [2,0,0,0,1,1,2,4,3,3]
-    tracks =[ BamTrack(bamfile), BedTrack(bedfile)]
     for t in tracks:
-        assert 10 == t.count()
-        assert 10 == len([i for i in t.fetch(interval)])
-        assert 6 == len([i for i in t.fetch(interval, strand="+")])
-        assert 4 == len([i for i in t.fetch(interval, strand="-")])
-        assert [list(interval) + bins] == [x for x in t.binned_stats(region, 10, split=True)]
-    
-    tracks += [WigTrack(wigfile), BigWigTrack(bwfile)]
-    for track in tracks:
-        profile = t.get_profile(interval)
-        assert interval[2] - interval[1] == len(profile)
-        assert 103 == sum(profile == 2)
-        assert 2 == max(profile)
-        assert 1 == min(profile)
-
-    bins = [2,0,0,0,1,1,2,2,2,2]
-    tracks = [BigWigTrack(bwfile)]
-    for t in tracks:
-        assert [list(interval) + bins] == [x for x in t.binned_stats(region, 10, split=True)]
+        if t.track_type == "feature":
+            assert 10 == len([i for i in t.fetch(interval)])
+            assert 6 == len([i for i in t.fetch(interval, strand="+")])
+            assert 4 == len([i for i in t.fetch(interval, strand="-")])
