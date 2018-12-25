f2ccfb7ba39311f5026e5accb6146fd7c212694b
shouldsee <shouldsee@github.com>
2018-12-25 17:10:34 +0000

    indent

--- fluff/plot.py
+++ fluff/plot.py
@@ -634,13 +634,13 @@ class AnnotationPanel(ProfilePanel):
                                 color=self.color,
                         )
                         ax.add_patch(arr)
-                if gstart > 0:
-		    labX = gstart - 0.01
-		    horizontalalignment = 'right'
-		else:
-		    labX = gend + 0.01
-		    horizontalalignment = 'left'
-                ax.text(labX, h_gene, genename,
+        if gstart > 0:
+            labX = gstart - 0.01
+            horizontalalignment = 'right'
+        else:
+            labX = gend + 0.01
+            horizontalalignment = 'left'
+        ax.text(labX, h_gene, genename,
                             horizontalalignment= horizontalalignment,
                             verticalalignment="center",
                             fontproperties=font)
